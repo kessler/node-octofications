3b1640a222b34ab3d48c3a9715157c9023642e9a
Michael Rubanov <mrubanov@gmail.com>
2014-05-18 18:39:48 +0300

    linting & formating

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-var config = require('./lib/config.js')
-var feed = require('./lib/feed.js')
-var FeedParser = require('feedparser')
-var Notifier = require('node-notifier')
-var lruCache = require('lru-cache')
-var debug = require('debug')('octofications')
-var schedule = require('tempus-fugit').scheduling.schedule
-var inspect = require('util').inspect
-var itemToMessage = require('./lib/itemToMessage.js')
+var config = require('./lib/config.js');
+var feed = require('./lib/feed.js');
+var FeedParser = require('feedparser');
+var Notifier = require('node-notifier');
+var lruCache = require('lru-cache');
+var debug = require('debug')('octofications');
+var schedule = require('tempus-fugit').scheduling.schedule;
+var inspect = require('util').inspect;
+var itemToMessage = require('./lib/itemToMessage.js');
 
-var EventEmitter = require('events').EventEmitter
-var notifier = new Notifier()
- 
-var options = { maxAge: 1000 * 60 * 60 * 24, max: 1000 }
-var cache = lruCache(options)
-var feedEmitter = new EventEmitter()
-var firstLaunch = true
+var EventEmitter = require('events').EventEmitter;
+var notifier = new Notifier();
+
+var options = { maxAge: 1000 * 60 * 60 * 24, max: 1000 };
+var cache = lruCache(options);
+var feedEmitter = new EventEmitter();
+var firstLaunch = true;
 
 feedEmitter.on('item', function (item, count) {
-	
-	if (!cache.get(item.title)) {
-		debug('feed item %s, %s, fl: %s', item.title, item.link, firstLaunch)
-	
-		if (!firstLaunch) {
-			setTimeout(function () {			
-				notifier.notify(itemToMessage(item))	
-			}, count * 1000)
-		}
-
-		cache.set(item.title, true)
-	}
-
-})
+    "use strict";
+    if (!cache.get(item.title)) {
+        debug('feed item %s, %s, fl: %s', item.title, item.link, firstLaunch);
+        if (!firstLaunch) {
+            setTimeout(function () {
+                notifier.notify(itemToMessage(item));
+            }, count * 1000);
+        }
+        cache.set(item.title, true);
+    }
+});
 
 feedEmitter.on('end', function () {
-	firstLaunch = false
-})
-	
-var job = schedule(config.pollInterval, feed(feedEmitter))
+    "use strict";
+    firstLaunch = false;
+});
+var job = schedule(config.pollInterval, feed(feedEmitter));
 
-debug('feed job scheduled, %s', inspect(job))
+debug('feed job scheduled, %s', inspect(job));
