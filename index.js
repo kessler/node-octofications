4acf7ff001471a69dbb2311512c7a5a6d6e38d77
yaniv kessler <yanivk@gmail.com>
2015-02-01 11:10:00 +0200

    update node-notifier and tempus-fugit

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 var config = require('./lib/config.js');
-var feed = require('./lib/feed.js');
+var pollFeed = require('./lib/pollFeed.js');
 var FeedParser = require('feedparser');
-var Notifier = require('node-notifier');
+var notifier = require('node-notifier');
 var lruCache = require('lru-cache');
 var debug = require('debug')('octofications');
-var schedule = require('tempus-fugit').scheduling.schedule;
+var schedule = require('tempus-fugit').schedule;
 var inspect = require('util').inspect;
 var itemToMessage = require('./lib/itemToMessage.js');
 
 var EventEmitter = require('events').EventEmitter;
-var notifier = new Notifier();
 
-var options = { maxAge: 1000 * 60 * 60 * 24, max: 1000 };
+var options = {
+	maxAge: 1000 * 60 * 60 * 24,
+	max: 1000
+};
+
 var cache = lruCache(options);
 var feedEmitter = new EventEmitter();
 var firstLaunch = true;
 
-feedEmitter.on('item', function (item, count) {
-    "use strict";
-    if (!cache.get(item.title)) {
-        debug('feed item %s, %s, fl: %s', item.title, item.link, firstLaunch);
-        if (!firstLaunch) {
-            setTimeout(function () {
-                notifier.notify(itemToMessage(item));
-            }, count * 1000);
-        }
-        cache.set(item.title, true);
-    }
+feedEmitter.on('item', function(item, count) {
+	if (!cache.get(item.title)) {
+		debug('feed item %s, %s, fl: %s', item.title, item.link, firstLaunch);
+
+		// avoid the swamp of old feed items when launching 1st time
+		//if (!firstLaunch) {
+			setTimeout(function() {			
+				notifier.notify(itemToMessage(item));
+			}, count * 5000);
+		//}
+
+		cache.set(item.title, true);
+	}
 });
 
-feedEmitter.on('end', function () {
-    "use strict";
-    firstLaunch = false;
+feedEmitter.on('end', function() {
+
+	firstLaunch = false;
 });
-var job = schedule(config.pollInterval, feed(feedEmitter));
+
+var job = schedule(config.pollInterval, pollFeed(feedEmitter));
 
 debug('feed job scheduled, %s', inspect(job));
